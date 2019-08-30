72b179e28788523cd72ad26c8a19bf1df54d8762
Ang Sherpa <angsherpa232@gmail.com>
2019-08-30 11:07:14 +0200

    accordion height adjusted to fit the content. Twitter link added. login/support button removed from navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -127,23 +127,6 @@ const Navbar = class extends React.Component {
                   <FormattedMessage id="generic.License" />
                 </a>
                 <span className="h-rule" />
-                <Link className="navbar-item" to="/">
-                  <FormattedMessage id="generic.navbar.Support" />
-                </Link>
-                <Link className="navbar-item" to="/">
-                  <FormattedMessage id="generic.navbar.Login" />
-                  <svg
-                    className="icon is-small is-blue"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M416 448h-84c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h84c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32h-84c-6.6 0-12-5.4-12-12V76c0-6.6 5.4-12 12-12h84c53 0 96 43 96 96v192c0 53-43 96-96 96zm-47-201L201 79c-15-15-41-4.5-41 17v96H24c-13.3 0-24 10.7-24 24v96c0 13.3 10.7 24 24 24h136v96c0 21.5 26 32 41 17l168-168c9.3-9.4 9.3-24.6 0-34z"
-                    ></path>
-                  </svg>
-                </Link>
               </div>
               <img
                 className="mobileBackgroundImageBottom"
@@ -153,25 +136,7 @@ const Navbar = class extends React.Component {
               <div className="mobile-menu-bottom-space" />
             </div>
             <div id="navMenu" className="navbar-menu d-none d-md-block">
-              <div className="navbar-end has-text-centered">
-                <Link className="navbar-item" to="/">
-                  <FormattedMessage id="generic.navbar.Support" />
-                </Link>
-                <Link className="navbar-item" to="/">
-                  <FormattedMessage id="generic.navbar.Login" />
-                  <svg
-                    className="icon is-small is-white"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M416 448h-84c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h84c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32h-84c-6.6 0-12-5.4-12-12V76c0-6.6 5.4-12 12-12h84c53 0 96 43 96 96v192c0 53-43 96-96 96zm-47-201L201 79c-15-15-41-4.5-41 17v96H24c-13.3 0-24 10.7-24 24v96c0 13.3 10.7 24 24 24h136v96c0 21.5 26 32 41 17l168-168c9.3-9.4 9.3-24.6 0-34z"
-                    ></path>
-                  </svg>
-                </Link>
-              </div>
+              <div className="navbar-end has-text-centered"></div>
             </div>
           </div>
         </nav>
