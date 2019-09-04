c3ee0458e6733a9954108444cf38d53b4f176d12
oterral <oterral@users.noreply.github.com>
2019-09-04 17:26:06 +0200

    Allow login via iframexframe (#8)

--- src/utils/oidcsettings.js
+++ src/utils/oidcsettings.js
@@ -19,5 +19,19 @@ const Oidcsettings = {
   automaticSilentRenew: false,
   loadUserInfo: true
 };
+/*
+
+const Oidcsettings = {
+  authority: "https://sso.mapset.ch/openid",
+  client_id: "576922",
+  redirect_uri: "https://editor.mapset.ch/signin",
+  post_logout_redirect_uri: "https://editor.mapset.ch/signout",
+  silent_redirect_uri: "http://localhost:3000/silent",
+  response_type: "id_token token",
+  scope: "openid profile email roles",
+  filterProtocolClaims: true,
+  automaticSilentRenew: false,
+  loadUserInfo: true
+};*/
 
 export default Oidcsettings;
