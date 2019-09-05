4c281823f5bbd25f8694677f8de756452b932042
Ang Sherpa <angsherpa232@gmail.com>
2019-09-05 11:16:07 +0200

    Logout icon added. Arror pointing to opposite direction. Oidc setting updated with plan editor configuration.

--- src/utils/oidcsettings.js
+++ src/utils/oidcsettings.js
@@ -8,8 +8,8 @@ const getUrl = path => {
 };
 
 const Oidcsettings = {
-  authority: "https://sso.mapset.ch/openid",
-  client_id: "023137",
+  authority: "https://sso.geops.io/openid",
+  client_id: "576922",
   redirect_uri: getUrl("/signin"),
   post_logout_redirect_uri: getUrl("/signout"),
   silent_redirect_uri: getUrl("/silent"),
@@ -19,19 +19,5 @@ const Oidcsettings = {
   automaticSilentRenew: false,
   loadUserInfo: true
 };
-/*
-
-const Oidcsettings = {
-  authority: "https://sso.mapset.ch/openid",
-  client_id: "576922",
-  redirect_uri: "https://editor.mapset.ch/signin",
-  post_logout_redirect_uri: "https://editor.mapset.ch/signout",
-  silent_redirect_uri: "http://localhost:3000/silent",
-  response_type: "id_token token",
-  scope: "openid profile email roles",
-  filterProtocolClaims: true,
-  automaticSilentRenew: false,
-  loadUserInfo: true
-};*/
 
 export default Oidcsettings;
