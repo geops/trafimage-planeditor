77c91f9f621471580d098a0a128f2255bafc26f7
elias <elias.englen@geops.de>
2020-01-30 12:59:01 +0100

    styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = class extends React.Component {
     return (
       <IntlProvider locale={this.state.locale} messages={this.state.messages}>
         <footer className="footer">
-          <div className="expander" <a className="button" href="http://eepurl.com/gRlRvv ">Datenschutz</a> 
+          <div className="expander" ><a className="button" href="http://eepurl.com/gRlRvv ">Datenschutz</a> 
           </div>
           <div>
             <span className="brandName">Trafimage </span>
