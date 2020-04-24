b5574efe1bcd7e6a30fe8e155c23345f8a8db7cc
Özkan Yanikbas <oezkan.yanikbas@geops.de>
2020-04-24 15:13:21 +0200

    Add Tracking Dialog

---
+++ src/components/TrackingDialog.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import Cookies from 'js-cookie'
+
+import '../sass/tracker.sass'
+
+export default function TrackingDialog () {
+  
+  const [tracking, setTracking] = useState(Cookies.get('tracking'))
+  
+  function acceptTracking () {
+    Cookies.set('tracking', true)
+    setTracking(Cookies.get('tracking'))
+  }
+
+  return (
+    !tracking || tracking === undefined ?
+    <div className="tracking-wrapper">
+      <div className="tracking-window">
+        <section className="text">
+        Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.
+        </section>
+        <section className="actions">
+          <button onClick={() => { window.location = 'https://geops.ch/node/182?language=en'}}>Mehr Informationen</button>
+          <button onClick={acceptTracking}>Akzeptieren</button>
+        </section>
+      </div>
+    </div>
+    : ''
+  )  
+}
