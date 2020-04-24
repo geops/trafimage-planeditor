e0a89b857678ce8ac0c135f2cba0fedfc60cbd8a
Özkan Yanikbas <oezkan.yanikbas@geops.de>
2020-04-24 14:54:43 +0200

    Cahnge Tabtitle

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ var proxy = require("http-proxy-middleware");
 
 module.exports = {
   siteMetadata: {
-    title: "Trafimage MapSet",
+    title: "Trafimage mapset",
     description: "CMS onePager for GeOps"
   },
   plugins: [
