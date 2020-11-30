08e96457beb3c41283a7348ad2662026d6693efa
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2020-11-30 13:18:26 +0100

    Add missing locale and user props to Layout component.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Error404SVG } from "@geops/geops-ui";
 import Layout from "../components/Layout";
+import userManager from "../utils/userManager";
 
 import "../sass/404.sass";
 
-const NotFoundPage = () => (
-  <Layout>
-    <div className="notfound-container">
-      <img src={Error404SVG} alt="Error 404 - Page not found" />
-      <a href="/">
-        <button className="btn blue-btn">Startseite</button>
-      </a>
-    </div>
-  </Layout>
-);
+const NotFoundPage = ({ pageContext: { locale } }) => {
+  const [user, setUser] = useState(null);
+
+  if (typeof window !== "undefined" && userManager) {
+    userManager.events.addUserLoaded((userr) => {
+      setUser(userr);
+    });
+  }
+
+  return (
+    <Layout locale={locale} user={user}>
+      <div className="notfound-container">
+        <img src={Error404SVG} alt="Error 404 - Page not found" />
+        <a href="/">
+          <button className="btn blue-btn">Startseite</button>
+        </a>
+      </div>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
